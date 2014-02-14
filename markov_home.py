3eb211c2b9d11d5dbb77657b63081aa829aef5a9
Yasmin Khan <yazzzz@gmail.com>
2014-02-13 19:54:20 -0800

    fixed punctuation and removed double quotes

---
+++ markov_home.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import sys
+import pprint
+import random
+
+def make_chains(text):
+    """Takes an input text as a string and returns a dictionary of
+    markov chains."""
+    text_list = text.split()
+    text_dict = {}
+
+    for i in range(2,len(text_list)):
+        bgram1 = text_list[i-2].strip('\"')
+        bgram2 = text_list[i-1].strip('\"')
+        value = text_list[i].strip('\"')
+        if not text_dict.get((bgram1, bgram2)):
+            text_dict[bgram1, bgram2] = [value]
+        else:
+            text_dict[bgram1, bgram2] += [value]
+    #pprint.pprint(text_dict)
+    return text_dict
+
+def pick_first_random_key(chains):
+    random_key = (random.sample(chains.keys(), 1))
+    while not random_key[0][0].istitle():
+        random_key = (random.sample(chains.keys(), 1))
+    return random_key        
+
+
+def make_text(chains):
+    """Takes a dictionary of markov chains and returns random text
+    based off an original text."""
+    #get random key from dictionary and add it to list
+    random_key = pick_first_random_key(chains)
+    sentence = [random_key[0][0],random_key[0][1]]
+
+    while chains.get(random_key[0]) and len(" ".join(sentence)) < 135: #while our key exists in the dict
+        pick_value = chains[random_key[0]][random.randint(0, len(chains[random_key[0]])-1)]
+        print pick_value
+        #make new bigram with y value from random_key and pick_value 
+        sentence.append(pick_value)
+
+        result =  "\n" +  " ".join(sentence)
+        print result[-1]
+        if result[-1].isalnum():
+            result += "."
+        random_key = [(random_key[0][1], pick_value)]
+   
+    return result
+   
+
+def main():
+    #args = sys.argv[1]
+    script, filename  = sys.argv 
+    # Change this to read input_text from a file
+    f = open(filename)
+    input_text = f.read()
+
+    chain_dict = make_chains(input_text)
+    random_text = make_text(chain_dict)
+    print random_text
+
+if __name__ == "__main__":
+    main()
